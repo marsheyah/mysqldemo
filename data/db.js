0f660b3570fb5449f3cb3b24addcf5b3e0973eed
Marsheyah Antonyraj <marsheyah004@gmail.com>
2026-02-06 10:04:06 +0530

    Initial commit

--- data/db.js
+++ data/db.js
@@ -1,9 +1,13 @@
 const mysql = require("mysql2");
+const dotenv = require("dotenv");
+dotenv.config();
 const connection = mysql.createConnection({
-    host :"localhost",
-    user : "root",
-    password : "anu9342778668",
-    database : "demo_kec"
+   host: process.env.DB_HOST,
+   user: process.env.DB_USER,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_NAME,
+   port: process.env.DB_PORT
+
 });
 connection.connect((err)=>{
     if(err){
